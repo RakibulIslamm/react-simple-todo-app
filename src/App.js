bbb85283392c654290d1498bbba51a6398247e96
RakibulIslam <rakibulislamsofficial@gmail.com>
2022-05-19 01:40:32 +0600

    Not found page added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Route, Routes } from 'react-router-dom';
 import Login from './Components/Login/Login';
+import PageNotFound from './Components/PageNotFound/PageNotFound';
 import PrivateRoute from './Components/PrivateRoute/PrivateRoute';
 import TodoApp from './Components/TodoApp/TodoApp';
 
@@ -11,6 +12,7 @@ function App() {
           <TodoApp />
         </PrivateRoute>}></Route>
         <Route path='/login' element={<Login />}></Route>
+        <Route path='*' element={<PageNotFound />}></Route>
       </Routes>
     </div>
   );
