8d9f8be1fd666bb95de943825297fb1072484344
RakibulIslam <rakibulislamsofficial@gmail.com>
2022-05-18 20:45:20 +0600

    react confirm alert added

---
+++ src/Components/Alert/Alert.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import 'react-confirm-alert/src/react-confirm-alert.css';
+
+const Alert = ({ onClose, handleAlert }) => {
+    return (
+        <div className='bg-gray-900 w-[400px] xs:w-11/12 xs:mx-auto p-10 text-white'>
+            <h1 className='text-2xl font-semibold'>Are you sure?</h1>
+            <p className='text-lg'>You want to <b className='text-xl'>delete</b> this item?</p>
+            <div className='flex items-center gap-2 pt-4'>
+                <button className='px-6 py-1 border-2 hover:bg-white hover:text-black transition-all duration-200 ease-in-out border-white' onClick={() => handleAlert(onClose, true)}>Yes</button>
+                <button className='px-6 py-1 border-2 hover:bg-white hover:text-black transition-all duration-200 ease-in-out border-white' onClick={() => handleAlert(onClose, false)}>No</button>
+            </div>
+        </div>
+    );
+};
+
+export default Alert;
--- src/Components/TodoApp/TodoApp.js
+++ src/Components/TodoApp/TodoApp.js
@@ -1,16 +1,48 @@
 import React from 'react';
 import TodoCard from '../TodoCard/TodoCard';
 import TodoForm from '../TodoForm/TodoForm';
+import { toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { confirmAlert } from 'react-confirm-alert';
+import Alert from '../Alert/Alert';
 
 const TodoApp = () => {
+
+
+
+    const handleDelete = (id) => {
+        const handleAlert = (close, confirmation) => {
+
+            if (confirmation) {
+                console.log('Deleted');
+                toast.success('List deleted successfully')
+            }
+            else {
+                console.log('not deleted');
+                close();
+            }
+
+            close();
+        }
+        confirmAlert({
+            customUI: ({ onClose }) => {
+                return (
+                    <Alert onClose={onClose} handleAlert={handleAlert} />
+                )
+            }
+        });
+    }
+
+
     return (
         <>
             <section class="min-h-screen dark:bg-gray-800 dark:text-gray-50 flex">
                 <TodoForm />
                 <div className='p-10 space-y-5'>
-                    <TodoCard />
+                    <TodoCard handleDelete={handleDelete} />
                 </div>
             </section>
+            <ToastContainer />
         </>
     );
 };
